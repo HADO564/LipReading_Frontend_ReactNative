a56cf845f7eede7f51a2592acf6b29b8197c1260
Haider Muhammad <70658014+HADO564@users.noreply.github.com>
2024-04-29 03:14:51 +0500

    plugin attempt

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native";
-
-import React from "react";
-import { useCameraPermission, Camera } from "react-native-vision-camera";
+import React, { useEffect, useState } from "react";
+import { useCameraPermission, Camera, runAsync } from "react-native-vision-camera";
 import { useCameraDevice } from "react-native-vision-camera";
 import { useFrameProcessor } from "react-native-vision-camera";
+import {Buffer} from "buffer";
+import io from "socket.io-client";
+import { runOnJS } from "react-native-reanimated";
+import { useSharedValue } from "react-native-worklets-core";
+import { Worklets } from "react-native-vision-camera";
+import { socketSend } from "./processPlugin";
+import { VisionCameraProxy,Frame } from "react-native-vision-camera";
+import {xyz} from "./Plugin";
+
+
+const SERVER_URL = "ws://10.7.237.84:5000";
+
 
 export default function App() {
+
   const device = useCameraDevice("back");
   const { hasPermission, requestPermission } = useCameraPermission();
+  const {frames, setFrames} = useSharedValue();
+  const [socket, setSocket] = useSharedValue(null);
+  const [sending, setSending] = useState(false);
+
+
+  const sendFramesToServer = (frames) => {
+    "worklet"
+    console.log(frames.pixelFormat)
+    console.log("Sending frames to server:", frames);
+    socket.emit("frames", {frames:frames});
+  };
 
   const frameProcessor = useFrameProcessor((frame) => {
     "worklet";
-    console.log(
-      `Frame ${frame.pixelFormat}`
-    );
+    console.log("Frame:", frame.toString());
+  
+    //use socketSend here
+    console.log(xyz(frame));
+  
   });
+
+  useEffect(() => {
+    // Establish WebSocket connection
+    const newSocket = io(SERVER_URL);
+    setSocket(newSocket);
+
+    return () => {
+      // Close WebSocket connection when component unmounts
+      newSocket.close();
+    };
+  }, []);
+
+
+
 
   if (!hasPermission) {
     return (
@@ -34,11 +73,22 @@ export default function App() {
       </View>
     );
   }
-
+  
+// useEffect(()=>{setFramesBuffer([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])},[])
   return (
-    <Camera isActive={true} device={device} style={StyleSheet.absoluteFill} pixelFormat="rgb" frameProcessor={frameProcessor} />
+    <View style={styles.container}>
+      <Camera
+        isActive={true}
+        device={device}
+        style={StyleSheet.absoluteFill}
+        pixelFormat="rgb"
+        frameProcessor={frameProcessor}
+      />
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
---
+++ index.js
@@ -0,0 +1,7 @@
+const plugin = VisionCameraProxy.initFrameProcessorPlugin('xyz');
+
+export function xyz(frame) {
+    'worklet'
+      return plugin.call(frame);
+    }
+
