fb9eb38032ba1b4fbc4ba82dd636ac031f569851
Haider Muhammad <70658014+HADO564@users.noreply.github.com>
2024-04-28 22:52:47 +0500

    Initial commit

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native";
+
+import React from "react";
+import { useCameraPermission, Camera } from "react-native-vision-camera";
+import { useCameraDevice } from "react-native-vision-camera";
+import { useFrameProcessor } from "react-native-vision-camera";
 
 export default function App() {
+  const device = useCameraDevice("back");
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    "worklet";
+    console.log(
+      `Frame ${frame.pixelFormat}`
+    );
+  });
+
+  if (!hasPermission) {
+    return (
+      <View style={styles.container}>
+        <Text>No permission</Text>
+        <Button onPress={requestPermission} title="Request permission" />
+      </View>
+    );
+  }
+
+  if (!device) {
+    return (
+      <View style={styles.container}>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Camera isActive={true} device={device} style={StyleSheet.absoluteFill} pixelFormat="rgb" frameProcessor={frameProcessor} />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
 module.exports = function(api) {
+  plugins: [
+    ['react-native-worklets-core/plugin'],
+  ],
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
